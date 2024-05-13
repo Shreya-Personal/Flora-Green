744e0ff9500d5bc3398960f30341872fd29cdc6c
Shreya-Personal <141000874+Shreya-Personal@users.noreply.github.com>
2024-05-13 14:36:14 +1000

    PWM wave function changed to be more reliable

--- Testing/m5core2PinTester/AnalogPinTester.ino
+++ Testing/m5core2PinTester/AnalogPinTester.ino
@@ -23,20 +23,33 @@ int samplePin(int PWM_PIN){
   startTime = millis();  // Get the current time
   totalHighTime = 0;
   totalCount = 0;
-
+  int totalval = 0; 
   // Sample the PWM signal for the specified duration
   while (millis() - startTime < SAMPLE_DURATION_MS) {
-    int pwmValue = digitalRead(PWM_PIN);  // Read digital value (HIGH or LOW) of PWM pin
+    int pwmValue = digitalRead(PWM_PIN);
     if (pwmValue == HIGH) {
       totalHighTime += 1;  // Increment high time counter
     }
     totalCount += 1;  // Increment total count
+    
     delayMicroseconds(10);  // Optional small delay for stability
   }
 
   // Calculate duty cycle as percentage (0-100)
   float dutyCycle = (float)totalHighTime / totalCount * 100.0;
   return dutyCycle;
+}
+
+int GetPWM(int pin)
+{
+  unsigned long highTime = pulseIn(pin, HIGH, 50000UL);  // 50 millisecond timeout
+  unsigned long lowTime = pulseIn(pin, LOW, 50000UL);  // 50 millisecond timeout
+
+  // pulseIn() returns zero on timeout
+  if (highTime == 0 || lowTime == 0)
+    return digitalRead(pin) ? 100 : 0;  // HIGH == 100%,  LOW = 0%
+
+  return (100 * highTime) / (highTime + lowTime);  // highTime as percentage of total cycle time
 }
 
 void setup() {
@@ -47,14 +60,15 @@ void setup() {
 }
 
 void loop() {
-  
-  float dutyCycle = samplePin(LED_PIN);
+  int PWM = GetPWM(LED_PIN);
   // Map the duty cycle to a 0-10 range
-  int led_brightness = round(dutyCycle/10);
+  int led_brightness = map(PWM, 0, 100, 0, 10);
+  int dutyCycle = samplePin(PETAL_PIN);
 
-  dutyCycle = samplePin(PETAL_PIN);
-  int petal = round(dutyCycle/100); 
-
+  int petal = map(dutyCycle, 0, 30, 0, 1); 
+  if (petal > 1) { 
+    petal = 1; 
+  }
   Serial.print("%, led_brightness: ");
   Serial.println(led_brightness);
   Serial.print("%, Petal: ");
